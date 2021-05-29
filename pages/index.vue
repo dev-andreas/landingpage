98c5f60e847f8e9fa27048365d9e0432401faa5b
Andreas Gerasimow <66901404+dev-andreas@users.noreply.github.com>
2021-05-29 11:37:19 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@ export default {
   head: {
     title: "Webentwickler und Freelancer aus Ulm.",
     htmlAttrs: {
-      lang: "en",
+      lang: "de",
     },
     meta: [
       { charset: "utf-8" },
